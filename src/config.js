4f847f4aecc9d8fe3b25ecb6276b2e835c8b8ae3
Ashwini More <amore@officebrain.com>
2017-11-09 16:42:50 +0530

    E-commerce search_filter

--- src/config.js
+++ src/config.js
@@ -1,20 +1,5 @@
-module.exports = {
-	// LAN IpAddress
-    // baseURL : 'http://172.16.230.84:3030',
-
-    // WiFi ipAddress
-    baseURL : 'http://172.16.230.134:3030',
-
-    // Local IpAddress
-    // baseURL : 'http://localhost:3030'
-
-    // Local ipaddress
-    // Wifi
-    ipAddress : 'http://172.16.230.134',
-    // LAN
-    // ipAddress : 'http://172.16.230.84',
-    // Local
-    // ipAddress : 'http://localhost',
-    
-    pluginsPath : '/home/software/AllProjects/FlowzBuilder/src/assets/plugins'
+ module.exports = {
+    // baseURL : 'http://172.16.230.84:3030'
+    baseURL : 'http://localhost:3030'
 }
+
