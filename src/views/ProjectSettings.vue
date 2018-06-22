fded9e0babf7820088c2b3f6498567e64607ac91
gkumar9 <gkumar@officebrain.com>
2018-06-22 17:57:26 +0530

    updated img

--- src/views/ProjectSettings.vue
+++ src/views/ProjectSettings.vue
@@ -461,7 +461,7 @@
                   <div class="row">
                     <div class="col-md-4">
                       <div class="thumbnail">
-                        <img src="http://res.cloudinary.com/flowz/image/upload/v1528348629/builder/images/flowz-website-template.png" alt="flowz-template" class="img-responsive template-image" @click="revertToCMSTemplate(template = 'flowz-website')"/>
+                        <img src="https://res.cloudinary.com/flowz/image/upload/v1529670155/builder/images/Webp.png" alt="flowz-template" class="img-responsive template-image" @click="revertToCMSTemplate(template = 'flowz-website')"/>
                       <a href="#" target="_blank" class="view-template"><i class="fa fa-search"></i></a>
                       <!-- <button class="btn btn-primary btn-lg btn-block" @click="revertToTemplate(template = 'web1')">Template 1</button> -->
                       </div>
