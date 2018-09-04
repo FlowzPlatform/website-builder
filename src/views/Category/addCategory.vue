e3afbc5d09997b416f50b9d3aa47b949c3e4ffa0
Software <ppriyank@officebrain.com>
2018-09-04 18:30:20 +0530

    113 - onchange website listing

--- src/views/Category/addCategory.vue
+++ src/views/Category/addCategory.vue
@@ -253,8 +253,7 @@ export default {
             else {
               this.formItem.name = this.fdata.categorySlug;
               this.formItem.website = this.fdata.website;
-            } 
-            this.onChangeWebsite(this.formItem.website);            
+            }             
         }).catch(err => {
             console.log(err);
         })
