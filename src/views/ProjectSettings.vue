f5a8817a54e33145cd5b594a82de665085d1ede8
gkumar9 <gkumar@officebrian.com>
2018-09-28 12:54:28 +0530

    saving progress

--- src/views/ProjectSettings.vue
+++ src/views/ProjectSettings.vue
@@ -4309,10 +4309,8 @@ export default {
                         arrayofpartials = arrayofpartials.concat(gitrepoPartials.data)
                         count = count + 1
                     } while (gitrepoPartials.data.length == 20)
-                    console.log('all partials from git:',arrayofpartials)
                     let actualpartials = Object.keys(self.settings[2].layoutOptions[0])
                     for (let i = 0; i < actualpartials.length; i++) {
-                        console.log('partial:',actualpartials[i])
                         if (actualpartials[i] != 'Layout'&& actualpartials[i]!='Menu') {
                             let partialindex = _.findIndex(arrayofpartials, function(o) {
                               // console.log('git name:',name)
@@ -4320,7 +4318,6 @@ export default {
                             })
                             // console.log('partialindex:',partialindex)
                             if (partialindex == -1) {
-                                console.log('partials not found')
                                 for (let j = 0; j < self.settings[2].layoutOptions[0][actualpartials[i]].length; j++) {
                                     let partialpagefilecontent = await axios.get(config.baseURL + '/flows-dir-listing/0?path=' + self.folderUrl + '/Partials/' + actualpartials[i] + '/' + self.settings[2].layoutOptions[0][actualpartials[i]][j].label + '.partial', {}).catch((e) => {
                                         console.log(e)
@@ -4330,7 +4327,6 @@ export default {
 
                                 }
                             } else {
-                                console.log('partials found')
                                 let gitrepoPartialspages = []
                                 let arrayofpartialspages = []
                                 let countpartial = 1
@@ -4339,26 +4335,25 @@ export default {
                                     arrayofpartialspages = arrayofpartialspages.concat(gitrepoPartialspages.data)
                                     count = count + 1
                                 } while (gitrepoPartialspages.data.length == 20)
-                                // console.log('arrayofpartialspages:',arrayofpartialspages)
                                 let actualpartialpages=self.settings[2].layoutOptions[0][actualpartials[i]]
                                 // for(let j=0;j<actualpartialpages.length;j++){
                                   for(let j in actualpartialpages){
-                                    console.log(j)
                                     let partialpageindex=_.findIndex(arrayofpartialspages,function(o){
-                                      o.name=j.label+'.partial'
+                                      return o.name == actualpartialpages[j].label+'.partial'
                                     })
-                                      let partialpagefilecontent=await axios.get(config.baseURL + '/flows-dir-listing/0?path=' + self.folderUrl + '/Partials/' + actualpartials[i] + '/' + j.label + '.partial', {}).catch((e) => {
+                                      let partialpagefilecontent=await axios.get(config.baseURL + '/flows-dir-listing/0?path=' + self.folderUrl + '/Partials/' + actualpartials[i] + '/' + actualpartialpages[j].label + '.partial', {}).catch((e) => {
                                         console.log(e)
                                     })
+                                    let tempjson=''
                                     if(partialpageindex==-1){
-                                      let tempjson = '{"action": "create","encoding":"base64","file_path": "Partials/' + actualpartials[i] + '/' + j.label + '.partial' + '","content": "' + Base64.btoa(unescape(encodeURIComponent(partialpagefilecontent.data))) + '" }'
-                                    totalarray.push(tempjson)
+                                       tempjson = '{"action": "create","encoding":"base64","file_path": "Partials/' + actualpartials[i] + '/' + actualpartialpages[j].label + '.partial' + '","content": "' + Base64.btoa(unescape(encodeURIComponent(partialpagefilecontent.data))) + '" }'
+                                    
                                     }
                                     else{
-                                      let tempjson = '{"action": "update","encoding":"base64","file_path": "Partials/' + actualpartials[i] + '/' + j.label + '.partial' + '","content": "' + Base64.btoa(unescape(encodeURIComponent(partialpagefilecontent.data))) + '" }'
-                                    totalarray.push(tempjson)
+                                       tempjson = '{"action": "update","encoding":"base64","file_path": "Partials/' + actualpartials[i] + '/' + actualpartialpages[j].label + '.partial' + '","content": "' + Base64.btoa(unescape(encodeURIComponent(partialpagefilecontent.data))) + '" }'
+                                    
                                     }
-
+                                    totalarray.push(tempjson)
 
                                 }
 
@@ -4366,7 +4361,7 @@ export default {
 
                         }
                     }
-                    let buildpayload='{ "branch": "'+branchName+'","commit_message": "'+commitMessage+'", "actions": ['+totalarray+'] }'
+                  let buildpayload='{ "branch": "'+branchName+'","commit_message": "'+commitMessage+'", "actions": ['+totalarray+'] }'
                   let axiosoptioncommit={
                     method:'post',
                     url:'https://gitlab.com/api/v4/projects/'+this.newRepoId+'/repository/commits',
